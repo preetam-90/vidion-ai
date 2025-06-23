ebba8db34bd7fbdf08acf5f9c622b45c294f834e
preetam-90 <preetamkumar8873@gmail.com>
2025-06-24 00:14:31 +0530

    Update OpenRouter API key

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 // Remove dotenv import and config
-export const OPENROUTER_API_KEY = 'sk-or-v1-free-basic-7yrzd37q9qiuzvq6zcgb5dfl9jxq548nkwi1n3dy';
+export const OPENROUTER_API_KEY = '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';
 // Using DuckDuckGo API which doesn't require an API key
